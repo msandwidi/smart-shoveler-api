71d9608c488a352e61837d1e9116cc4a631c2e37
unknown <shoudou@outlook.com>
2019-11-17 18:26:16 -0600

    cleanup order routes

--- controlers/order.js
+++ controlers/order.js
@@ -1,43 +1,37 @@
 const Order = require("../models/Order");
 const utils = require("../utils");
 
-const get_ping = (req, res) => {
-	res.status(200).json({
-		success: true,
-		text: "Up and Running..."
-	});
-};
-
-const get_landing = (req, res) => {
-	res.status(200).json({
-		text: "Smart Shoveler API home...",
-		success: true
-	});
-};
-
 /**
  * user post a work request
  * @param {*} req 
  * @param {*} res 
  */
 const post_add_order = async (req, res) => {
-	const { email, description, date, type, isRecurrent } = req.body;
+	try {
+		const { email, description, date, type, isRecurrent } = req.body;
 
-	let order = new Order({
-		email,
-		description,
-		type,
-		date,
-		isRecurrent,
-		confirmation: utils.generateRandomToken("number", 6)
-	});
+		let order = new Order({
+			email,
+			description,
+			type,
+			date,
+			isRecurrent,
+			confirmation: utils.generateRandomToken("number", 6)
+		});
 
-	order = await order.save();
+		order = await order.save();
 
-	res.status(200).json({
-		success: true,
-		order
-	});
+		res.status(200).json({
+			success: true,
+			order
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			success: false,
+			message: "An unknown error occured"
+		});
+	}
 };
 
 /**
@@ -46,33 +40,63 @@ const post_add_order = async (req, res) => {
  * @param {*} res 
  */
 const put_update_order = async (req, res) => {
-	const { email, description, date, type, isRecurrent } = req.body;
+	try {
+		const { email, description, date, type, isRecurrent } = req.body;
 
-	const number = req.params.id;
+		const number = req.params.id;
 
-	let order = await Order.findOne({
-		number
-	});
+		let order = await Order.findOne({
+			number
+		});
 
-	if (!order) {
-		res.status(404).json({
+		if (!order) {
+			res.status(404).json({
+				success: false,
+				message: "We could not find your order"
+			});
+		}
+
+		order.email = email;
+		order.description = description;
+		order.data = date;
+		order.isRecurrent = isRecurrent;
+		order.type = type;
+
+		order = await order.save();
+
+		res.status(200).json({
+			success: true,
+			order
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
 			success: false,
-			message: "We could not find your order"
+			message: "An unknown error occured"
 		});
 	}
+};
 
-	order.email = email;
-	order.description = description;
-	order.data = date;
-	order.isRecurrent = isRecurrent;
-	order.type = type;
+/**
+ * get current active works
+ * @param {*} req 
+ * @param {*} res 
+ */
+const get_orders = async (req, res) => {
+	try {
+		const orders = await Order.find({ isDeleted: false });
 
-	order = await order.save();
-
-	res.status(200).json({
-		success: true,
-		order
-	});
+		res.status(200).json({
+			success: true,
+			orders
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			success: false,
+			message: "An unknown error occured"
+		});
+	}
 };
 
 /**
@@ -81,28 +105,36 @@ const put_update_order = async (req, res) => {
  * @param {*} res 
  */
 const post_search_order = async (req, res) => {
-	const { email, orderNumber: number } = req.body;
+	try {
+		const { email, orderNumber: number } = req.body;
 
-	let order = await Order.findOne({
-		number,
-		email
-	});
+		let order = await Order.findOne({
+			number,
+			email
+		});
 
-	if (!order) {
-		res.status(404).json({
+		if (!order) {
+			res.status(404).json({
+				success: false,
+				message: "We could not find your order"
+			});
+		}
+
+		res.status(200).json({
+			success: true,
+			order
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
 			success: false,
-			message: "We could not find your order"
+			message: "An unknown error occured"
 		});
 	}
-
-	res.status(200).json({
-		success: true,
-		order
-	});
 };
 
 module.exports = (app) => {
-	app.get("/api/v1/orders", get_landing);
+	app.get("/api/v1/orders", get_orders);
 	app.post("/api/v1/orders", post_add_order);
 	app.put("/api/v1/orders/:id", put_update_order);
 	app.post("/api/v1/orders/search", post_search_order);
