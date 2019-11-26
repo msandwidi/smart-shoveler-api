d7cccf4124b8a882038c7dcf89847a162aab1587
msandwidi <shoudou@outlook.com>
2019-11-26 15:23:28 -0600

    fixed fetch message schema

--- controllers/message.js
+++ controllers/message.js
@@ -11,6 +11,22 @@ const get_my_messages = async (req, res) => {
   });
 };
 
+const post_new_message = async (req, res) => {
+  const user = req.user._id;
+  const { recipientId, content } = req.body;
+  const message = UserMessage({
+    recipientId,
+    senderId: user,
+    content
+  });
+
+  res.status(200).json({
+    success: true,
+    message
+  });
+};
+
 module.exports = app => {
   app.get("/api/v1/messages", authenticate, get_my_messages);
+  app.post("/api/v1/messages", authenticate, post_new_message);
 };
