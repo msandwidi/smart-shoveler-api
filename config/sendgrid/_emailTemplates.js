579873d4798a9efe0c6c069dc4fbd089ed7c236a
unknown <shoudou@outlook.com>
2019-11-27 00:08:21 -0600

    added recovery screen

--- config/sendgrid/_emailTemplates.js
+++ config/sendgrid/_emailTemplates.js
@@ -1,12 +1,13 @@
 let user_templateIds = {};
 
-user_templateIds.CONFIRM_SIGNUP = "d-9f9451b3fc1c4dd4a700ac207af38635";
-user_templateIds.RECOVERY_LINK = "d-6913403049684f1fb80521985153b6ce";
-user_templateIds.CONFIRM_RECOVERY = "d-6913403049684f1fb80521985153b6ce";
-user_templateIds.CONFIRM_ACTIVATION = "d-6913403049684f1fb80521985153b6ce";
-user_templateIds.WELCOME_MESSAGE = "d-6913403049684f1fb80521985153b6ce";
-user_templateIds.CONFIRM_PROFILE_UPDATE = "d-6913403049684f1fb80521985153b6ce";
-user_templateIds.CONFIRM_PASSWORD_CHANGE = "d-6913403049684f1fb80521985153b6ce";
+user_templateIds.CONFIRM_SIGNUP = "d-6913403049684f1fb80521985153b6ce";
+user_templateIds.RECOVERY_LINK = "d-6bbb514db7234e299c26558838c23d09";
+user_templateIds.CONFIRM_RECOVERY = "d-5aba54d2589143f39ae4d84f0b78da27";
+user_templateIds.CONFIRM_ACTIVATION = "d-0cda3b563c65457a8eea9dd57c14d6af";
+user_templateIds.WELCOME_MESSAGE = "d-d6e1392990924136ac5384130acba4d5";
+user_templateIds.CONFIRM_PROFILE_UPDATE = "d-fed967665b784d06b0b06a1c52e5c0dd";
+user_templateIds.CONFIRM_WORK_REQUEST_SUBMITED= "d-4e5586be68254df3a8412bf5c3cf7523";
+user_templateIds.CONFIRM_ACCOUNT_UPGRADE= "d-4e5586be68254df3a8412bf5c3cf7523";
 
 module.exports = {
   ...user_templateIds
