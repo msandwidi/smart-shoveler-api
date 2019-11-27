40ee430aae76cc56ca6a82186e0647aa27cbc761
unknown <shoudou@outlook.com>
2019-11-26 22:48:15 -0600

    added new sendgrid keys

--- config/sendgrid/_emailTemplates.js
+++ config/sendgrid/_emailTemplates.js
@@ -1,6 +1,6 @@
 let user_templateIds = {};
 
-user_templateIds.CONFIRM_SIGNUP = "d-6913403049684f1fb80521985153b6ce";
+user_templateIds.CONFIRM_SIGNUP = "d-9f9451b3fc1c4dd4a700ac207af38635";
 user_templateIds.RECOVERY_LINK = "d-6913403049684f1fb80521985153b6ce";
 user_templateIds.CONFIRM_RECOVERY = "d-6913403049684f1fb80521985153b6ce";
 user_templateIds.CONFIRM_ACTIVATION = "d-6913403049684f1fb80521985153b6ce";
